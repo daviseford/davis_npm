039f3f5eed418fe6b7a356ba6441839308437e1d
Davis Ford <ford_davis@bah.com>
2016-04-22 20:09:31 -0400

    dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,26 @@
 /**
  * Created by loaner on 4/22/16.
  */
-module.exports = {};
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        './app/index.js'
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel_loader'}
+        ]
+    }
+    output: {
+        filename: 'index_bundle.js',
+        path: __dirname + '/app/out'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
